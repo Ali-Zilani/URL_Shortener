11e9846c906c9ee920ae1c25975d27cadfc35078
Ali Zilani <ma21chb0b43@student.nitw.ac.in>
2025-08-11 18:45:49 +0530

    Initialised statefull Authentication and created User Model

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ const app = express();
 const PORT = 3001;
 const urlRouter = require("./routes/url.js");
 const { connectToMongoDB } = require("./dbConnection");
-const URL = require("./models/url.js");
 const path = require("path");
 const staticRoute = require("./routes/staticRouter.js");
 // DB connection
 connectToMongoDB();
 
-//Home Route
-// app.get("/", (req, res) => {
-//   req.name = "Mohammad Ali Zilani"; // adding stuff to request
-//   return res
-//     .status(200)
-//     .json({ message: `Welcome to the URL Shortener API made by ${req.name}` });
-// });
 //SSR: Server Side Rendering
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
